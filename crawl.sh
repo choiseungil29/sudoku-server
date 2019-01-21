40c6c61206c1cb9129208a8d38f1549ec02f333f
clogic <choiseungil29@gmail.com>
2019-01-21 17:05:56 +0000

    add to async

---
+++ crawl.sh
@@ -0,0 +1,4 @@
+export LC_ALL=en_US.UTF-8
+
+. /home/sichoi/workspace/sudoku-server/venv/bin/activate
+python3 /home/sichoi/workspace/sudoku-server/crawler.py
--- crawler.py
+++ crawler.py
@@ -1,13 +1,20 @@
+import aiohttp
 import requests
 import random
+import asyncio
 
 from models import Puzzle
 from db import session
 
-def crawl():
+async def crawl():
   levels = ['easy', 'medium', 'hard', 'expert']
   level = random.choice(levels)
-  r = requests.get(f'https://sudoku.com/api/getLevel/{level}').json()
+  print('go')
+
+  http = aiohttp.ClientSession()
+  resp = await http.get(f'https://sudoku.com/api/getLevel/{level}')
+  r = await resp.json()
+  print(r)
 
   problem = r['desc'][0]
   answer = r['desc'][1]
@@ -15,15 +22,27 @@ def crawl():
   print(f'problem : {problem}')
   print(f'answer : {answer}')
 
+  await http.close()
+
   return {
     'level': level,
     'problem': r['desc'][0],
     'answer': r['desc'][1]
   }
 
-if __name__ == '__main__':
 
-  for i in range(0, 10):
-    puzzle = Puzzle(**crawl())
-    session.add(puzzle)
-  session.commit()
+async def crawl_all():
+  fts = [asyncio.ensure_future(crawl()) for _ in range(0, 50)]
+
+  for f in asyncio.as_completed(fts):
+    try:
+      puzzle = await f
+      puzzle = Puzzle(**puzzle)
+      session.add(puzzle)
+      session.commit()
+    except Exception as e:
+      pass
+
+if __name__ == '__main__':
+  loop = asyncio.get_event_loop()
+  loop.run_until_complete(crawl_all())
